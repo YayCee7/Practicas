a3284ff9798ca3d1429f535b0e18679290eba57b
YayCee7 <julioachram@gmail.com>
2021-02-24 19:52:15 -0400

    introducción a js

---
+++ javascript testing/js/app.js
@@ -0,0 +1,44 @@
+// log de consola: esto te ayuda a mostrar mensajes en cosola.
+
+/* console.log("IDK")
+console.log("XD")
+console.log("COOL")
+console.log('HELLO')
+console.log('WORLD')
+console.log(2) */ //se pueden dejar sms, numeros, ecuaciones y variables.
+
+// alerta para el usuario
+
+/* alert('Bienvenido a mi test de js') */
+
+var cajita = 'ñapato' // variable tipo: texto.
+cajita = 2+2
+
+var gumball = cajita 
+
+console.log(gumball)
+
+// sumar: 23, 45.
+
+var num1 = 23
+var num2 = 45
+var resultado = num1+num2
+
+console.log(resultado)
+
+// entrada de información
+
+var nombre = prompt('Hola, como te llamas?')
+
+alert('Hola, '+nombre+' buen dia.')
+
+console.log(nombre)
+
+// sumar X num (min:2) mostrar en alert el resultado
+
+var numx = parseInt(prompt('ingrese el primer numero')) // prompt se usa para guardar respuesta en texto.
+var numy = parseInt(prompt('sumar con:')) // parseInt convierte el texto en numeros enteros.
+
+alert(numx+numy)
+
+console.log(numx+numy)
